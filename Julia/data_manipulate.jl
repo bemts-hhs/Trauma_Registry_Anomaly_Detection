c0783933d6ec9f326e1d2cc5dd3507f6a056a977
bemts-hhs <nicolas.foss@hhs.iowa.gov>
2026-04-09 15:48:46 -0500

    continued development of the project

--- Julia/data_manipulate.jl
+++ Julia/data_manipulate.jl
@@ -47,19 +47,20 @@ trauma_registry_counts_2020_2025_final = @chain trauma_registry_counts_2020_2025
         diff_2024 = `2024` - `2023`,
         diff_2025 = `2025` - `2024`,
     )
-    @relocate(diff_2021, after = `2021`)
-    @relocate(diff_2022, after = `2022`)
-    @relocate(diff_2023, after = `2023`)
-    @relocate(diff_2024, after = `2024`)
-    @relocate(diff_2025, after = `2025`)
     @mutate(
         mean_records = round(mean.(c(`2020`, `2021`, `2022`, `2023`, `2024`, `2025`)), digits = 3),
         var_records = round(var.(c(`2020`, `2021`, `2022`, `2023`, `2024`, `2025`)), digits = 3),
         sd_records = round(std.(c(`2020`, `2021`, `2022`, `2023`, `2024`, `2025`)), digits = 3),
-        mean_diff = round(mean.(c(`diff_2021`, `diff_2022`, `diff_2023`, `diff_2024`, `diff_2025`)), digits = 3),
-        var_diff = round(var.(c(`diff_2021`, `diff_2022`, `diff_2023`, `diff_2024`, `diff_2025`)), digits = 3),
-        sd_diff = round(std.(c(`diff_2021`, `diff_2022`, `diff_2023`, `diff_2024`, `diff_2025`)), digits = 3),
-        z_diff_2021 = diff_2021 / sd_diff
+        mean_diff = round(mean.(c(diff_2021, diff_2022, diff_2023, diff_2024, diff_2025)), digits = 3),
+        var_diff = round(var.(c(diff_2021, diff_2022, diff_2023, diff_2024, diff_2025)), digits = 3),
+        sd_diff = round(std.(c(diff_2021, diff_2022, diff_2023, diff_2024, diff_2025)), digits = 3)
+    )
+    @mutate(
+        z_score_diff_2021 = div(diff_2021, sd_diff),
+        z_score_diff_2022 = div(diff_2022, sd_diff),
+        z_score_diff_2023 = div(diff_2023, sd_diff),
+        z_score_diff_2024 = div(diff_2024, sd_diff),
+        z_score_diff_2025 = div(diff_2025, sd_diff)
     )
 end
 
@@ -72,19 +73,83 @@ diff_long =
         names_to = :year,
         values_to = :diff
     )
-    @mutate(year = replace.(year, "diff_" => ""))
+    @mutate(
+        year = replace.(year, "diff_" => ""),
+        sign = ifelse(diff .< 0, "negative", "positive")
+    )
 end
 
 # Plots
-p_box =
-    ggplot(diff_long) +
-    geom_boxplot(aes(x = :facility, y = :diff)) +
-    facet_wrap(:facility; ncol = 4) +
-    labs(
-        x = "Facility",
-        y = "Differences",
-        title = "Distribution of Year‑to‑Year Differences by Facility"
-    ) +
-    theme_ggplot2()
+# Get unique facility list
+facilities = unique(diff_long.facility)
 
-draw_ggplot(p_box)
+# For loop to make plots for each facility to examine 
+# the distribution of their reporting differences over the
+# years
+for f in facilities
+
+    # subset current facility
+    df_f = diff_long[diff_long.facility .== f, :]
+
+    # build raincloud plots
+    p_rain = ggplot(df_f, aes(x = :facility, y = :diff)) +
+    geom_rainclouds(
+        plot_boxplots = true,
+        show_boxplot_outliers = true,
+        show_median = true,
+        fill = :cyan,
+        size = 10,
+        stroke = 0
+    ) +
+    labs(
+        title = "Distribution of $f Reporting Differences 2021-2025",
+        x = "",
+        y = "Difference"
+    ) +
+    theme_minimal()
+    
+    # save each raincloud plot
+    ggsave(p_rain, "plots/boxplots/diff_boxplot_$(f).png")
+
+end
+
+# move on to a loop for the column plots
+for f in facilities
+
+    # subset current facility
+    df_f = diff_long[diff_long.facility .== f, :]
+
+    # create column plots
+    p_col = ggplot(df_f, aes(x = :year, y = :diff)) +
+    geom_col(strokecolor = :black, strokewidth = 1, fill = :lightgray) +
+    labs(
+        title = "$f Reporting Differences 2021-2025",
+        x = "",
+        y = "Difference"
+    ) + 
+    theme_minimal()
+
+    # save each plot
+    ggsave(p_col, "plots/columnplots/diff_columnplot_$(f).png")
+end
+
+# plot all differences over the years to assess the distribution
+plot_all_diffs = ggplot(diff_long, aes(x = :year, y = :diff)) +
+    geom_rainclouds(
+        plot_boxplots = true,
+        show_boxplot_outliers = true,
+        show_median = true,
+        color = :blue,
+        fill = :coral,
+        size = 5,
+        stroke = 0
+        ) +
+    labs(
+        title = "Distribution of Reporting Differences Across Facilities",
+        x = "",
+        y = "Difference"
+    ) +
+    theme_minimal()
+
+    # save plot
+    ggsave(plot_all_diffs, "plots/diffplots_all/diffplots_all.png"; width = 1200, height = 700)
--- Julia/setup.jl
+++ Julia/setup.jl
@@ -1,4 +1,4 @@
-###_____________________________________________________________________________
+``###_____________________________________________________________________________
 # Setup for the anomaly detection project to leverage Tidier and DotEnv
 # Setup for the environment variables to be used to secure filepaths
 ###_____________________________________________________________________________
@@ -34,10 +34,10 @@ end
 DotEnv.load!()
 
 # Assign environment variables (no paths yet)
-iowa_trauma_registry_counts_path_2020 = DotEnv.ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2020"]
-iowa_trauma_registry_counts_path_2021 = DotEnv.ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2021"]
-iowa_trauma_registry_counts_path_2022 = DotEnv.ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2022"]
-iowa_trauma_registry_counts_path_2023 = DotEnv.ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2023"]
-iowa_trauma_registry_counts_path_2024 = DotEnv.ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2024"]
-iowa_trauma_registry_counts_path_2025 = DotEnv.ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2025"]
-iowa_trauma_registry_counts_path_2026 = DotEnv.ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2026"]
+iowa_trauma_registry_counts_path_2020 = ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2020"]
+iowa_trauma_registry_counts_path_2021 = ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2021"]
+iowa_trauma_registry_counts_path_2022 = ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2022"]
+iowa_trauma_registry_counts_path_2023 = ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2023"]
+iowa_trauma_registry_counts_path_2024 = ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2024"]
+iowa_trauma_registry_counts_path_2025 = ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2025"]
+iowa_trauma_registry_counts_path_2026 = ENV["IOWA_TRAUMA_REGISTRY_COUNTS_PATH_2026"]

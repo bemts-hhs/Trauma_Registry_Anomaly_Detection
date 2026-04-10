78dc58f6a69510ab812a80936d5477982840eb6b
bemts-hhs <nicolas.foss@hhs.iowa.gov>
2026-04-10 16:14:00 -0500

    continued development of the project including more advanced anomaly detection for registry counts

--- Julia/data_manipulate.jl
+++ Julia/data_manipulate.jl
@@ -5,19 +5,19 @@
 # union all rows from files and select the needed variables
 trauma_registry_counts_2020_2025 = @chain trauma_registry_counts_2020 begin
     @bind_rows(
-    trauma_registry_counts_2021, 
-    trauma_registry_counts_2022, 
-    trauma_registry_counts_2023, 
-    trauma_registry_counts_2024, 
-    trauma_registry_counts_2025
+        trauma_registry_counts_2021,
+        trauma_registry_counts_2022,
+        trauma_registry_counts_2023,
+        trauma_registry_counts_2024,
+        trauma_registry_counts_2025
     )
     @select(year, facility, total)
 end
 
 # pivot columns wider
-trauma_registry_counts_2020_2025_pivot = @chain trauma_registry_counts_2020_2025 begin 
+trauma_registry_counts_2020_2025_pivot = @chain trauma_registry_counts_2020_2025 begin
     @pivot_wider(
-        names_from = year, 
+        names_from = year,
         values_from = total
     )
     @mutate(
@@ -56,6 +56,13 @@ trauma_registry_counts_2020_2025_final = @chain trauma_registry_counts_2020_2025
         sd_diff = std.(c(diff_2021, diff_2022, diff_2023, diff_2024, diff_2025))
     )
     @mutate(
+        pct_2021 = (diff_2021) / `2020`,
+        pct_2022 = (diff_2022) / `2021`,
+        pct_2023 = (diff_2023) / `2022`,
+        pct_2024 = (diff_2024) / `2023`,
+        pct_2025 = (diff_2025) / `2024`
+    )
+    @mutate(
         z_score_diff_2021 = (diff_2021 - mean_diff) / sd_diff,
         z_score_diff_2022 = (diff_2022 - mean_diff) / sd_diff,
         z_score_diff_2023 = (diff_2023 - mean_diff) / sd_diff,
@@ -63,37 +70,73 @@ trauma_registry_counts_2020_2025_final = @chain trauma_registry_counts_2020_2025
         z_score_diff_2025 = (diff_2025 - mean_diff) / sd_diff
     )
     @mutate(
-        anomaly_2021 = abs(z_score_diff_2021) >= 1.5,
-        anomaly_2022 = abs(z_score_diff_2022) >= 1.5,
-        anomaly_2023 = abs(z_score_diff_2023) >= 1.5,
-        anomaly_2024 = abs(z_score_diff_2024) >= 1.5,
-        anomaly_2025 = abs(z_score_diff_2025) >= 1.5
-    )
+        z_anomaly_2021 = abs(z_score_diff_2021) >= 2,
+        z_anomaly_2022 = abs(z_score_diff_2022) >= 2,
+        z_anomaly_2023 = abs(z_score_diff_2023) >= 2,
+        z_anomaly_2024 = abs(z_score_diff_2024) >= 2,
+        z_anomaly_2025 = abs(z_score_diff_2025) >= 2,
+        pct_anomaly_2021 = abs(pct_2021) >= 1,
+        pct_anomaly_2022 = abs(pct_2022) >= 1,
+        pct_anomaly_2023 = abs(pct_2023) >= 1,
+        pct_anomaly_2024 = abs(pct_2024) >= 1,
+        pct_anomaly_2025 = abs(pct_2025) >= 1
+		)
     @mutate(
-        any_anomaly = any(c(
-            anomaly_2021,
-            anomaly_2022,
-            anomaly_2023,
-            anomaly_2024,
-            anomaly_2025
-    ))
-)
+        any_z_anomaly = any(c(
+            z_anomaly_2021,
+            z_anomaly_2022,
+            z_anomaly_2023,
+            z_anomaly_2024,
+            z_anomaly_2025,
+        )),
+		any_pct_anomaly = any(c(
+            pct_anomaly_2021,
+            pct_anomaly_2022,
+            pct_anomaly_2023,
+            pct_anomaly_2024,
+            pct_anomaly_2025,
+        ))
+    )
 end
 
 # Before additional data manipulation and modeling, plot differences
 diff_long =
-@chain trauma_registry_counts_2020_2025_final begin
-    @select(facility, diff_2021, diff_2022, diff_2023, diff_2024, diff_2025)
-    @pivot_longer(
-        diff_2021:diff_2025,
-        names_to = :year,
-        values_to = :diff
-    )
-    @mutate(
-        year = replace.(year, "diff_" => ""),
-        sign = ifelse(diff .< 0, "negative", "positive")
-    )
-end
+    @chain trauma_registry_counts_2020_2025_final begin
+        @select(facility, contains(r"^(diff|pct)_\d{4}$"))
+        @pivot_longer(
+            contains(r"^(diff|pct)_\d{4}$"),
+            names_to = :year,
+            values_to = :diff
+        )
+        @mutate(
+            measure = year,
+            year = replace.(year, r"pct_|diff_" => ""),
+            sign = ifelse(diff .< 0, "negative",
+                ifelse(diff .> 0, "positive", "neutral"),
+            ))
+    end
+
+# plot the distribution of differences
+diff_distribution_plot = ggplot(
+                             subset(diff_long, :measure => m -> occursin.("diff", m))
+                         ) +
+                         geom_density(@aes(x = diff, fill = year)) +
+                         facet_wrap(:year, scales="free") +
+                         theme_minimal()
+
+draw_ggplot(diff_distribution_plot, (1000, 800))
+
+# plot the distribution of the percent differences
+pct_diff_distribution_plot = ggplot(
+                                 subset(diff_long, :measure => m -> occursin.("pct", m),
+                                     :diff => d -> isfinite.(d)
+                                 )
+                             ) +
+                             geom_density(@aes(x = diff, fill = year)) +
+                             facet_wrap(:year, scales="free") +
+                             theme_minimal()
+
+draw_ggplot(pct_diff_distribution_plot, (1000, 800))
 
 # Plots
 # Get unique facility list
@@ -105,25 +148,26 @@ facilities = unique(diff_long.facility)
 for f in facilities
 
     # subset current facility
-    df_f = diff_long[diff_long.facility .== f, :]
+    df_f = subset(diff_long, :facility => x -> x .== f, :measure => m -> occursin.("diff", m))
 
     # build raincloud plots
-    p_rain = ggplot(df_f, aes(x = :facility, y = :diff)) +
-    geom_rainclouds(
-        plot_boxplots = true,
-        show_boxplot_outliers = true,
-        show_median = true,
-        fill = :cyan,
-        size = 10,
-        stroke = 0
-    ) +
-    labs(
-        title = "Distribution of $f Reporting Differences 2021-2025",
-        x = "",
-        y = "Difference"
-    ) +
-    theme_minimal()
-    
+    p_rain =
+        ggplot(df_f, aes(x=:facility, y=:diff)) +
+        geom_rainclouds(
+            plot_boxplots=true,
+            show_boxplot_outliers=true,
+            show_median=true,
+            fill=:cyan,
+            size=10,
+            stroke=0,
+        ) +
+        labs(
+            title="Distribution of $f Reporting Differences 2021-2025",
+            x="",
+            y="Difference",
+        ) +
+        theme_minimal()
+
     # save each raincloud plot
     ggsave(p_rain, "plots/boxplots/diff_boxplot_$(f).png")
 
@@ -133,39 +177,42 @@ end
 for f in facilities
 
     # subset current facility
-    df_f = diff_long[diff_long.facility .== f, :]
+    df_f = subset(diff_long, :facility => x -> x .== f, :measure => m -> occursin.("diff", m))
 
     # create column plots
-    p_col = ggplot(df_f, aes(x = :year, y = :diff)) +
-    geom_col(strokecolor = :black, strokewidth = 1, fill = :lightgray) +
-    labs(
-        title = "$f Reporting Differences 2021-2025",
-        x = "",
-        y = "Difference"
-    ) + 
-    theme_minimal()
+    p_col =
+        ggplot(df_f) +
+        geom_hline(yintercept=0, color=:darkgray, linestyle=:solid) +
+        geom_col(aes(x=:year, y=:diff), strokewidth=1, fill=:cyan) +
+        labs(
+            title="$f Reporting Differences 2021-2025",
+            x="",
+            y="Difference",
+        ) +
+        theme_minimal()
 
     # save each plot
     ggsave(p_col, "plots/columnplots/diff_columnplot_$(f).png")
 end
 
 # plot all differences over the years to assess the distribution
-plot_all_diffs = ggplot(diff_long, aes(x = :year, y = :diff)) +
+plot_all_diffs =
+    ggplot(diff_long, aes(x=:year, y=:diff)) +
     geom_rainclouds(
-        plot_boxplots = true,
-        show_boxplot_outliers = true,
-        show_median = true,
-        color = :blue,
-        fill = :coral,
-        size = 5,
-        stroke = 0
-        ) +
+        plot_boxplots=true,
+        show_boxplot_outliers=true,
+        show_median=true,
+        color=:blue,
+        fill=:coral,
+        size=5,
+        stroke=0,
+    ) +
     labs(
-        title = "Distribution of Reporting Differences Across Facilities",
-        x = "",
-        y = "Difference"
+        title="Distribution of Reporting Differences Across Facilities",
+        x="",
+        y="Difference",
     ) +
     theme_minimal()
 
-    # save plot
-    ggsave(plot_all_diffs, "plots/diffplots_all/diffplots_all.png"; width = 1200, height = 700)
+# save plot
+ggsave(plot_all_diffs, "plots/diffplots_all/diffplots_all.png"; width=1200, height=700)
--- Julia/setup.jl
+++ Julia/setup.jl
@@ -8,10 +8,12 @@ using Pkg
 # Ensure required packages are available
 Pkg.activate(".")
 Pkg.instantiate()
-Pkg.add(["Tidier", "TidierPlots", "DotEnv", "CSV", "DataFrames"])
+Pkg.add(["Tidier", "TidierPlots", "TidierDates", "DotEnv", "CSV", "DataFrames"])
 
 # Load packages
 using Tidier
+using TidierDates
+using Dates
 using TidierPlots
 using DotEnv
 using CSV
